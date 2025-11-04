e76ccc2a2812cf44bebafac169bed2609540c380
Berat Polat <119815205+BrplT0@users.noreply.github.com>
2025-11-05 02:05:35 +0300

    Add interactive Fibonacci sequence calculator
    Implements a command-line program that prompts the user for n and calculates the nth Fibonacci number, handling invalid input gracefully.

--- src/bin/s3_fibonacci.rs
+++ src/bin/s3_fibonacci.rs
@@ -1,1 +1,29 @@
-fn main() {}
+use std::io;
+fn main() {
+    let mut n = String::new();
+    println!("Enter the n:");
+    io::stdin().read_line(&mut n).unwrap();
+    let n: u128 = match n.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid number entered. Assuming n=0.");
+            0
+        }
+    };
+
+    if n == 0 {
+        println!("Last fib: 0");
+        return;
+    }
+
+    let mut num1 : u128 = 0;
+    let mut num2 : u128 = 1;
+
+    for _ in 0..n {
+        let new_fib : u128 = num1 + num2;
+        num1 = num2;
+        num2 = new_fib;
+    }
+
+    println!("Last fib: {}", num2);
+}
